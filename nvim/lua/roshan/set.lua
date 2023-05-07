2354c5e769585f00d3125c9a893d8c7e28a35439
vivianroshan <vivianroshan@gmail.com>
2023-05-07 12:36:27 +0530

    changed windows shell to powershell as well as changed the windows path of undodir

--- nvim/lua/roshan/set.lua
+++ nvim/lua/roshan/set.lua
@@ -4,11 +4,21 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.incsearch = true
+
+vim.opt.undofile = true
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    vim.opt.undodir      = os.getenv("HOMEPATH") .. "/.vim/undodir"
+    vim.opt.shell        = "pwsh";
+    vim.opt.shellcmdflag =
+    "-NoLogo -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering=[System.Management.Automation.OutputRendering]::PlainText;";
+    vim.opt.shellredir   = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    vim.opt.shellpipe    = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+    vim.opt.shellquote   = "";
+    vim.opt.shellxquote  = "";
+elseif vim.loop.os_uname().sysname == "Linux" then
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+end
