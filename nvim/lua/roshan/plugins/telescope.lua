45b8bbb0f079380b99fb47908c5768af9d5a9ebe
vivianroshan <vivianroshan@gmail.com>
2023-10-15 10:27:20 -0700

    Fall 2023 refresh

--- nvim/lua/roshan/plugins/telescope.lua
+++ nvim/lua/roshan/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
-        { 'nvim-lua/plenary.nvim', lazy = true },
+        { 'nvim-lua/plenary.nvim' },
     },
     cmd = 'Telescope',
     keys = {
